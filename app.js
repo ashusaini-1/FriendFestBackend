9b73a63e577471e9207055e408cdc35232118d9a
ashu <ashusaini17354@gmail.com>
2023-10-18 00:32:11 +0530

    commit

--- app.js
+++ app.js
@@ -3,11 +3,20 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const user = require("./routes/userRoutes");
 const location = require("./routes/locationRoutes");
+const cors=require('cors');
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
+
+const corsOptions = {
+    origin: 'https://friendfestapp.onrender.com', // Replace with the appropriate frontend URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  };
+  
+app.use(cors(corsOptions));
 app.use("/api/v1", user);
 app.use("/api/v1", location);
 
